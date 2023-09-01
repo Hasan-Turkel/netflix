8db388344390be55977995bc089a4486023e3920
hasan <mmhotmail>
2023-09-01 14:23:59 +0300

    firebase

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,11 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import AuthContextProvider from "@/context/AuthContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 //! metadata objesi ve generateMetadata fonksiyonu sadece Server Componentlerden export edilebilir.
 export const metadata = {
@@ -16,9 +17,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
-        <ToastContainer />
+        <AuthContextProvider>
+          <Navbar />
+          {children}
+          <ToastContainer />
+        </AuthContextProvider>
       </body>
     </html>
   );
